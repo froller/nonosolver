6ad0cfd5a93eef03ec50840ab5c029769904b97f
Александр Фролов <froller@froller.net>
2020-07-23 01:08:43 +0300

    Сломанный поиск гарантированно пустых клеток

--- solver.cpp
+++ solver.cpp
@@ -6,15 +6,28 @@ Solver::Solver(Nonogram *nonogram, Raster *raster): m_Nonogram(nonogram), m_Rast
     
 }
 
-void Solver::solve()
+bool Solver::solve()
+{
+    bool changed = false;
+    changed |= solveRows();
+    changed |= solveCols();
+    return changed;
+}
+
+bool Solver::solveRows()
+{
+    bool changed = false;
+    for (int y = 0; y < m_Nonogram->getHeight(); y++)
+        changed |= analyzeLine(eLine_Row, y);
+    return changed;
+}
+
+bool Solver::solveCols()
 {
     bool changed = false;
     for (int x = 0; x < m_Nonogram->getWidth(); x++)
         changed |= analyzeLine(eLine_Column, x);
-
-    for (int y = 0; y < m_Nonogram->getHeight(); y++)
-        changed |= analyzeLine(eLine_Row, y);
-
+    return changed;
 }
 
 bool Solver::analyzeLine(const eLine type, const unsigned short idx)
@@ -37,13 +50,15 @@ bool Solver::analyzeLine(const eLine type, const unsigned short idx)
         break;
     }
     
+    // Find and set persistant pixels
     changed |= findPersistantPixels(line, strips);
-    copyLineToRaster(type, idx, line);
     
-    // TODO: Find and set persistant pixels
-    // TODO: Find and mark unreacheable pixes
+    // Find and mark unreacheable pixes
+    changed |= findUnreachablePixels(line, strips);
+    
     // TODO: Find and mark bound pixels
     
+    copyLineToRaster(type, idx, line);
     return changed;
 }
 
@@ -76,11 +91,51 @@ bool Solver::findPersistantPixels(std::vector<char>& line, std::vector<unsigned 
     for (unsigned int i = 0; i < line.size(); i++)
         if (leftmost[i] && leftmost[i] == rightmost[i])
         {
+            changed = line[i] != leftmost[i];
             line[i] = leftmost[i];
-            changed = true;
         }
     return changed;
 }
+
+bool Solver::findUnreachablePixels (std::vector<char>& line, std::vector<unsigned short>& strips)
+{
+    std::vector<char> leftmost(line.size());
+    std::vector<char> rightmost(line.size());
+    
+    int pos;
+   
+    pos = 0;
+    for (unsigned int i = 0; i < strips.size(); i++)
+    {
+        for (int j = 0; j < strips[i]; j++)
+        {
+            leftmost[pos++] = i + 1;
+        }
+        pos++;
+    }
+    
+    pos = line.size() - 1;
+    for (int i = strips.size(); i > 0; i--)
+    {
+        for (int j = 0; j < strips[i - 1]; j++)
+            rightmost[pos--] = i;
+        pos--;
+    }
+
+    
+    bool changed = false;
+    // TODO fix unreachable pixel detection
+/*
+    for (unsigned int i = 0; i < line.size(); i++)
+        if (!leftmost[i] && !rightmost[i])
+        {
+            changed = line[i] != leftmost[i];
+            line[i] = -1;
+        }
+*/
+    return changed;
+}
+
 
 bool Solver::copyLineToRaster(const eLine type, const unsigned short idx, std::vector<char> &line)
 {
--- solver.h
+++ solver.h
@@ -16,10 +16,13 @@ class Solver
     bool analyzeLine(const eLine type, const unsigned short idx);
     //bool analyze(std::vector<unsigned char> strips, std::vector<char> line);
     bool findPersistantPixels(std::vector<char> &line, std::vector<unsigned short> &strips);
+    bool findUnreachablePixels(std::vector<char> &line, std::vector<unsigned short> &strips);
     bool copyLineToRaster(const eLine type, const unsigned short idx, std::vector<char> &line);
 public:
     Solver(Nonogram *nonogram, Raster *raster);
-    void solve();
+    bool solve();
+    bool solveRows();
+    bool solveCols();
 };
 
 #endif // __SOLVER_H__
